a25281ac05e35b52b6f339293053269e7de4bb74
Tyler Long <tyler4long@gmail.com>
2015-03-17 23:44:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# jquery-dist
+
+contains jquery.min.js
+
+
+## Installation
+
+`bower install jquery-dist`
+
+
+## For repository maintainers
+
+`fab update`
+
+update version number in bower.json
+
+```bash
+git tag v2.1.3
+git push origin v2.1.3
+```
---
+++ fabfile.py
@@ -0,0 +1,12 @@
+import re
+from subprocess import Popen, PIPE
+from fabric.api import local
+
+
+def update():
+    local('rm jquery.min.js')
+    output = Popen(["bower", "info", "jquery"], stdout = PIPE).communicate()[0]
+    latest_version = re.findall(r"version: '([2-9].\d.\d)',", output)[0]
+    print 'latest version: {0}'.format(latest_version)
+    local('wget http://code.jquery.com/jquery-{0}.min.js'.format(latest_version))
+    local('mv jquery-{0}.min.js jquery.min.js'.format(latest_version))
